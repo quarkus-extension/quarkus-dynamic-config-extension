65f49caec325b44b034e71ec9c88f150c222398d
gokhan.karadas <gokhan.karadas1992@gmail.com>
2020-08-05 18:31:15 +0300

    version 0.0.5

--- src/test/java/ConfigTestIT.java
+++ src/test/java/ConfigTestIT.java
@@ -1,14 +1,14 @@
-import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.Test;
-
-import static org.hamcrest.Matchers.is;
-
-@QuarkusTest()
-public class ConfigTestIT {
-
-    @Test
-    public void it_should_get_config() {
-        RestAssured.given().when().get("/config/age").then().body(is("28"));
-    }
-}
+//import io.quarkus.test.junit.QuarkusTest;
+//import io.restassured.RestAssured;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.hamcrest.Matchers.is;
+//
+//@QuarkusTest()
+//public class ConfigTestIT {
+//
+//    @Test
+//    public void it_should_get_config() {
+//        RestAssured.given().when().get("/config/age").then().body(is("28"));
+//    }
+//}
